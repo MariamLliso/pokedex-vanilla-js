9d2daa03ac7db09e80435e405bb9e4d9748d1a13
MariamLliso <contacto.mariamlliso@gmail.com>
2022-04-23 15:35:33 +0200

    PokeAPIService has been created for acces data easily

---
+++ public/js/services/PokeAPIService/PokeAPIService.js
@@ -0,0 +1,23 @@
+class PokeAPIService {
+  endpoint;
+
+  constructor() {
+    this.endpoint = "https://pokeapi.co/api/v2/pokemon/";
+  }
+
+  async getPokemonsPaginated(offset, limit = 10) {
+    const response = await fetch(
+      `${this.endpoint}?limit=${limit}&offset=${offset}`
+    );
+    const data = await response.json();
+    return data;
+  }
+
+  async getPokemonByName(name) {
+    const response = await fetch(`${this.endpoint}${name}`);
+    const data = await response.json();
+    return data;
+  }
+}
+
+export default PokeAPIService;
